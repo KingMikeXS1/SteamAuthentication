a27c1dfcc66cae367243c3267b18524eb4cfde79
KingMikeXS1 <78938662+KingMikeXS1@users.noreply.github.com>
2021-03-05 09:07:43 -0500

    Update SteamConfig.php
    Entered values into domainname and apikey

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 3.2
-$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = ""; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = "6B328D41EE66949204BBCEBA81C3852A"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "Achievement Arena"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = ""; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
